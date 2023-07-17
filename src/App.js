2964e92a2fbdf1b87de891ff9b11e746e3851abf
ZyesSubMod <azzouz.lyes.w@gmail.com>
2023-07-17 17:14:48 +0200

    browse router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./pages/Error";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home" element={<Home />}></Route>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Error" element={<Error />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
