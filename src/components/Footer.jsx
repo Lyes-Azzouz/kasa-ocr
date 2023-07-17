6916fe249ff7b579aaa8ec868e78631a2f954559
ZyesSubMod <azzouz.lyes.w@gmail.com>
2023-07-17 21:14:51 +0200

    maj

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "../styles/components/footer.scss";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <div className="footer">
-        <img src="public\images\logo_footer.png" alt="logo_footer" />
+      <div className="footer-content">
+        <img src="/images/logo_footer.png" alt="logo_footer" />
         <span>© 2020 Kasa. All rights reserved</span>
       </div>
     </div>
