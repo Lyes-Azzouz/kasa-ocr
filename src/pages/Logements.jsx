82ca990362b864303b9cf76c1e4a75cf766af822
ZyesSubMod <azzouz.lyes.w@gmail.com>
2023-07-18 04:34:14 +0200

    maj

---
+++ src/pages/Logements.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import LogementDetails from "../components/CardsContent";
+import Footer from "../components/Footer";
+
+const Logements = () => {
+  return (
+    <div>
+      <Navigation />
+      <LogementDetails />
+      <Footer />
+    </div>
+  );
+};
+
+export default Logements;
